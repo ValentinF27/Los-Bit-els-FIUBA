20583498bc9471a8dae1adc2626cf70e0a52f43f
matimilo5 <mmilocik@fi.uba.ar>
2025-12-12 15:22:29 -0300

    Add middleware for CORS and JSON parsing

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public')); 
