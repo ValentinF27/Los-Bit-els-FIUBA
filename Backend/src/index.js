84919bb516ef396e78e375d516ca6a3ad5f7562c
Valentin Fiasche <valentinfiasche@hotmail.com>
2025-12-15 02:50:52 -0300

    Se aumenta el tamaño de la query para mostrar usuario.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -11,15 +11,31 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend funcionando" });
 });
 
-app.get("/usuarios", async (req, res) => {
-    try {
-        const result = await pool.query("SELECT * FROM users");
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "DB error" });
+app.get("/usuarios/:id", async (req, res) => {
+  try {
+    // Tomamos el id desde la URL.
+    const { id } = req.params;
+
+    // Pedimos solo ese usuario a la base de datos.
+    const result = await pool.query(
+      "SELECT id, nickname, nom_completo, email, telefono, ubicacion, instrumento, genero_fav FROM usuarios WHERE id = $1",
+      [id]
+    );
+
+    // Si no existe, lanza mensaje de error.
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
     }
+
+    // Enviamos el usuario al frontend.
+    res.json(result.rows[0]);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "DB error" });
+  }
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
