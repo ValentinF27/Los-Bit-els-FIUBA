ff84f94d0f81d44253fd94c69f2793eadd9eeeb5
Valentin Fiasche <valentinfiasche@hotmail.com>
2025-12-15 19:52:16 -0300

    Se modulariza js del backend.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { pool } from "./db.js";
+
+import usuarios from "./routes/usuarios.js";
 
 const app = express();
 app.use(express.json());
@@ -11,31 +12,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend funcionando" });
 });
 
-app.get("/usuarios/:id", async (req, res) => {
-  try {
-    // Tomamos el id desde la URL.
-    const { id } = req.params;
-
-    // Pedimos solo ese usuario a la base de datos.
-    const result = await pool.query(
-      "SELECT id, nickname, nom_completo, email, telefono, ubicacion, instrumento, genero_fav FROM usuarios WHERE id = $1",
-      [id]
-    );
-
-    // Si no existe, lanza mensaje de error.
-    if (result.rows.length === 0) {
-      return res.status(404).json({ error: "Usuario no encontrado" });
-    }
-
-    // Enviamos el usuario al frontend.
-    res.json(result.rows[0]);
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "DB error" });
-  }
-});
-
+app.use('/api/usuarios', usuarios);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
