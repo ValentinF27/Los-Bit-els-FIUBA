96ad7d825be05408d84b53eb51468992eea72145
Valentin Fiasche <valentinfiasche@hotmail.com>
2025-12-18 01:05:43 -0300

    Envia reseña a Backend

--- Frontend/js/api.js
+++ Frontend/js/api.js
@@ -11,3 +11,33 @@ export async function getPartitura(id) {
   if (!res.ok) throw new Error(`Partitura no encontrada`);
   return await res.json();
 }
+
+// Envía una nueva reseña al backend.
+export async function enviarReseña({ partitura_id, titulo, contenido, estrellas }) {
+  // Tomamos usuario logueado desde localStorage
+  const usuario = JSON.parse(localStorage.getItem("usuarioLogueado"));
+  if (!usuario) throw new Error("Usuario no logueado");
+
+  const usuario_id = usuario.id; // ID del usuario que escribe la reseña
+
+  // Hacemos POST al backend para crear la reseña
+  const res = await fetch("http://localhost:3000/api/resenas", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      usuario_id,
+      partitura_id,
+      titulo,
+      contenido,
+      estrellas
+    })
+  });
+
+  // Revisamos si la petición falló
+  if (!res.ok) {
+    throw new Error("Error al enviar la reseña");
+  }
+  return true;
+}
+
+
