d55b86d1ee99fdd5b208b47196967362461bc5d0
Valentin Fiasche <valentinfiasche@hotmail.com>
2025-12-18 22:17:40 -0300

    Boton a perfil, aparece cuando usuario esta logueado.

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -11,20 +11,27 @@ export function logout() {
 export function initAuthUI() {
   const usuario = JSON.parse(localStorage.getItem("usuarioLogueado"));
 
+  const profileBtn = document.getElementById("profile-btn");
   const logoutBtn = document.getElementById("logout-btn");
   const loginBtn = document.getElementById("login-btn");
   const signupBtn = document.getElementById("signup-btn");
 
   if (usuario) {
-    // Usuario loguedo
+    // Usuario logueado
     if (logoutBtn) logoutBtn.style.display = "inline-block";
     if (loginBtn) loginBtn.style.display = "none";
     if (signupBtn) signupBtn.style.display = "none";
+    if (profileBtn) {
+      profileBtn.style.display = "inline-block";
+      // Redirigir al perfil del usuario
+      profileBtn.href = `usuario.html?id=${usuario.id}`;
+    }
   } else {
     // Usuario NO logueado
     if (logoutBtn) logoutBtn.style.display = "none";
     if (loginBtn) loginBtn.style.display = "inline-block";
     if (signupBtn) signupBtn.style.display = "inline-block";
+    if (profileBtn) profileBtn.style.display = "none";
   }
 }
 
